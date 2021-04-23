6642cf2a371e73fe59a2b4c94fcef1a3a50c7608
Dan <spqjeks@naver.com>
2021-04-23 23:28:59 +0900

    study go rutine

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/Dan-Doit/prectice-go/urlChecker"
 )
 
 func main() {
+	go gorutine("first")
+	go gorutine("second")
+	gorutine("third")
 	urlChecker.Checker()
+
+}
+
+func gorutine(word string) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second)
+		fmt.Println(word, i)
+	}
 }
